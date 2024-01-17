d3cc40ff63506010517fb8ee9e485ea5ab61a20a
cuong35327 <anhhhieuma55@gmail.com>
2024-01-17 09:56:02 +0700

    update code2

--- Assets/Playermoment.cs
+++ Assets/Playermoment.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Playermoment : MonoBehaviour
 {
@@ -12,8 +14,20 @@ public class Playermoment : MonoBehaviour
 
     float horizontal;
     bool isfasingRight = true;
-    float speed_jump= 5;
+    float speed_jump = 7;
     bool isjumpimg = false;
+
+    public float coin = 0;
+    public TextMeshProUGUI Cointext;
+
+
+    // máu
+    public int heath;
+    public int numofhearts;
+
+    public Image[] hearts;
+    public Sprite fullheart;
+    public Sprite emptyHears;
 
     void Start()
     {
@@ -26,26 +40,62 @@ public class Playermoment : MonoBehaviour
     {
         //di chuyển
         horizontal = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(horizontal * Speed,rb.velocity.y);
-        animator.SetFloat("runplayer",Mathf.Abs(horizontal));
+        rb.velocity = new Vector2(horizontal * Speed, rb.velocity.y);
+        animator.SetFloat("xvelocry", Mathf.Abs(rb.velocity.x));
+        animator.SetFloat("yvelocry", rb.velocity.y);
         //quay đầu
         Quaydau();
 
+
         //nhảy
-        if(Input.GetKeyDown(KeyCode.Space) && !isjumpimg)
+        if (Input.GetKeyDown(KeyCode.Space) && isjumpimg)
         {
-            rb.velocity = new Vector2(rb.velocity.x,speed_jump);
-            isjumpimg = true;
-            
+            rb.velocity = new Vector2(rb.velocity.x, speed_jump);
+            isjumpimg = false;
+
         }
 
-        animator.SetBool("isjump",isjumpimg);
-    
+        animator.SetBool("isjump", !isjumpimg);
+
+        if (!isjumpimg && Input.GetAxisRaw("Horizontal") != 0)
+        {
+            rb.velocity = new Vector2(horizontal * Speed, rb.velocity.y);
+        }
+
+
+        //Sử lý máu
+
+        if (heath > numofhearts)
+        {
+            heath = numofhearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < heath)
+            {
+                hearts[i].sprite = fullheart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHears;
+            }
+
+            if (i < numofhearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+
     }
 
     private void Quaydau()
     {
-        if(horizontal <0f && !isfasingRight || horizontal>0f && isfasingRight)
+        if (horizontal < 0f && !isfasingRight || horizontal > 0f && isfasingRight)
         {
             isfasingRight = !isfasingRight;
             Vector3 ls = transform.localScale;
@@ -56,15 +106,22 @@ public class Playermoment : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collider)
     {
-        Debug.Log("va cham vao "+ collider.gameObject.tag);
-        isjumpimg = false;
+        Debug.Log("va cham vao " + collider.gameObject.tag);
+        isjumpimg = true;
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        if(collider.gameObject.tag == "coin")
+        if (collider.gameObject.tag == "coin")
         {
+            coin++;
+            Cointext.SetText(coin.ToString());
             Destroy(collider.gameObject);
+        }
+
+        if (collider.gameObject.tag == "nam")
+        {
+            heath--;
         }
     }
 }
